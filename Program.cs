62334a2798f42bac4351be526b118b94aeaa6283
Midoriya <wladimi482002@gmail.com>
2022-08-31 12:21:16 +0300

    added generate xml document

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Xml.Linq;
+using System.Threading.Tasks;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -6,6 +8,20 @@ using System.Threading;
 
 namespace FileSystemScaner
 {
+    public class file_information
+    {
+        public string file_name { get; set; }
+        public string create_date { get; set; }
+        public string directory { get; set; }
+        public string size { get; set; }
+        public file_information(string file_name, string create_date, string directory, string size)
+        {
+            this.create_date = create_date;
+            this.directory = directory;
+            this.size = size;
+            this.file_name = file_name;
+        }
+    }
     public class Searcher
     {
         private DirectoryInfo directory_info;
@@ -22,24 +38,24 @@ namespace FileSystemScaner
             this.max_Create_time = max_Create_time;
             this.min_file_size = min_file_size;
             this.max_file_size = max_file_size;
-            this.find_feles = new List<FileInfo>();
+            this.find_feles = new List<file_information>();
         }
-        public List<FileInfo> find_feles;
+        public List<file_information> find_feles;
         /// <summary>
         /// use this to find all the files in the directory
         /// </summary>
         /// <param name="dir">durectory for finding</param>
         /// <returns>all file in the directory as list</returns>
-        public List<FileInfo> serach_files(DirectoryInfo dir)
+        public List<file_information> serach_files(DirectoryInfo dir)
         {
-            List<FileInfo> result = new List<FileInfo>();
+            List<file_information> result = new List<file_information>();
             dir.GetFiles(this.patternt_for_search, SearchOption.TopDirectoryOnly).ToList().ForEach(file => // get all files at directory
             {
                 try
                 {
                     if (file.CreationTime.Date >= this.min_create_time && file.CreationTime.Date <= this.max_Create_time && ((file.Length) / 1000) <= this.max_file_size && ((file.Length / 1000)) >= this.min_file_size)
                     {
-                        try { result.Add(file); } // adding to result array new file
+                        try { result.Add(new file_information(file.Name, file.CreationTime.ToString(), file.DirectoryName, (file.Length / 1024).ToString())); } // adding to result array new file
                         catch { Thread.Sleep(1); }; // if this file gives an error
                     }
                 }
@@ -69,14 +85,16 @@ namespace FileSystemScaner
 
 
     }
-
     internal class Program
     {
         public static int counter = 0;
-
+        public static List<file_information> file_list = new List<file_information>();
         static void Main(string[] args)
         {
+            var time_begin = DateTimeOffset.Now;
             List<DirectoryInfo> list_all_disc = new List<DirectoryInfo>();
+            XDocument document = new XDocument();
+            XElement root_list = new XElement("items");
 
             list_all_disc.Add(new DirectoryInfo("C:\\"));
 
@@ -113,26 +131,48 @@ namespace FileSystemScaner
                         });
                         if (endCount == 0) break;
                     }
-
                     search_list.ForEach(search => search.find_feles.ForEach(file =>
                     {
-                        Console.WriteLine($"{file.Name} : {file.DirectoryName} : {file.Length / 1024} : {file.CreationTime}");
+                        XElement root_class = new XElement("file");
+                        XElement filename = new XElement("filename", file.file_name);
+                        XElement create_date = new XElement("create-data", file.create_date);
+                        XElement directorys = new XElement("directory", file.directory);
+                        XElement file_size = new XElement("file-size", file.size);
+                        root_class.Add(filename);
+                        root_class.Add(create_date);
+                        root_class.Add(directorys);
+                        root_class.Add(file_size);
+                        root_list.Add(root_class);
                         counter++;
                     }));
                 }
 
                 // creating a new searches for find all files in the root directory
                 Searcher ls = new Searcher(directory, "*.*", DateTime.MinValue, DateTime.MaxValue, 0, Int32.MaxValue);
-                List<FileInfo> local_file = ls.serach_files(directory); // Search for files only in the root directory
+                List<file_information> local_file = ls.serach_files(directory); // Search for files only in the root directory
                 local_file.ForEach(file =>
                 {
-                    Console.WriteLine($"{file.Name} : {file.DirectoryName} : {file.Length / 1024} : {file.CreationTime}");
+                    XElement root_class = new XElement("file");
+                    XElement filename = new XElement("filename", file.file_name);
+                    XElement create_date = new XElement("create-data", file.create_date);
+                    XElement directorys = new XElement("directory", file.directory);
+                    XElement file_size = new XElement("file-size", file.size);
+                    root_class.Add(filename);
+                    root_class.Add(create_date);
+                    root_class.Add(directorys);
+                    root_class.Add(file_size);
+                    root_list.Add(root_class);
                     counter++;
                 });
                 Console.WriteLine($"Scan systme done. Find {counter} files");
+
                 if (counter == 0)
                     Console.WriteLine($"Scan systme Error. File not found");
             });
+            document.Add(root_list);
+            document.Save("123.xml");
+            var time_end = DateTimeOffset.Now;
+            Console.WriteLine($"{time_begin} | {time_end}");
             Console.ReadLine();
         }
     }
